b9de7edcca83567f3a00db0eace1bd7eb35b0f12
Richard Melchior <raustin.melchior@gmail.com>
2018-06-02 18:44:09 +0000

    automatically backed up by learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,13 +1,13 @@
 def unsafe?(speed)
 	if(speed < 40 || speed > 60)
-		return false
+		return false;
 	else
-		return true
+		return true;
 	end
 end
 
 
 
 def not_safe?(speed)
-	return speed < 40 || speed > 60 ? false : true
+	return speed < 40 || speed > 60 ? false : true;
 end
